2bc6a4985765a5da5fc1988a65ed5f990bb4ac25
Shazahanul Islam Shohag <shohag121@gmail.com>
2018-12-22 02:46:27 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBu9FHGOyOS_V-tHNOo3Ccqjz1oXzZjYfQ',
-    authDomain: 'getvirtualbooks.firebaseapp.com',
-    databaseURL: 'https://getvirtualbooks.firebaseio.com',
-    projectId: 'getvirtualbooks',
-    storageBucket: 'getvirtualbooks.appspot.com',
-    messagingSenderId: '342334744149'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   },
   aws: {
-    awsId: 'AKIAI3O2KXHP66MJJ5IQ',
-    awsSecret: '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',
-    awsTag: 'coderexpo02-20'
+    awsId: '',
+    awsSecret: '',
+    awsTag: ''
   }
 
 };
